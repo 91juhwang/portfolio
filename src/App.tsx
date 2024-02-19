92bee99a3c636297f01835ca95e1a59ceecc4434
James Hwang <91juhwang@gmail.com>
2024-02-19 11:36:10 -0500

    Center card

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,19 @@ function App() {
 
   return (
     <>
-      <div>
-        <Card heading='JAMES HWANG' body=''>
-        <p className="text-gray-700 text-base">
-          df
-        </p>
+      <div className='flex h-screen items-center justify-center'>
+        <Card>
+          <div className="px-6 py-4">
+            <h2 className="text-3xl font-bold mb-2">
+              JAMES HWANG
+            </h2>
+            <p className="text-gray-700 text-base">
+              (+1) 646-942-7592
+            </p>
+            <p className="text-gray-700 text-base">
+              91juhwang@gmail.com
+            </p>
+          </div>
           <div className="px-6 pt-4 pb-2">
             <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
             <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
