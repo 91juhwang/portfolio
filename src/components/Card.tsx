a9536ed2a42f06b80aa429e4d7b2cd08b059f63d
James Hwang <91juhwang@gmail.com>
2024-02-19 15:15:44 -0500

    Add icon svg

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -8,7 +8,7 @@ function Card(props: PropsWithChildren<CardProps>) {
   return (
     <>
       <div className={`max-w-sm rounded-lg overflow-hidden shadow-2xl ${props.padding} focus:ring-purple-500 focus:border-purple-500 border-purple-700 bg-white`}>
-        {props.children}  
+        {props.children}
       </div>
     </>
   )
